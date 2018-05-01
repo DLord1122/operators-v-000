397b14bfb09cf6df0a1420f708d1c4fea05f304f
Dennis Lord <lorddennis1006@gmail.com>
2018-05-01 22:59:41 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,12 @@
-def unsafe?(speed)
-if speed > 60 && speed < 40
-	true
-else speed < 40
-end
+def unsafe? speed
+  speed < 40 || speed > 60
 end
 
 
 
 def not_safe?(speed)
 
+  if speed < 40 || speed > 60 ? true : false
+
+  end
 end
